770ab459670b84119d61ec1715e1736e8f01dc66
Prateek Chaplot <prateekchaplot96@gmail.com>
2020-10-12 14:10:57 +0000

    Changes related to wikihow scraping - completed!

--- scraper/scraper/spiders/wikihow_spider.py
+++ scraper/scraper/spiders/wikihow_spider.py
@@ -1,24 +1,35 @@
 import scrapy
-import numpy as np
+# import numpy as np
 
 class WikihowScraper(scrapy.Spider):
     name = 'wikihow'
     start_urls = [
+        'https://www.wikihow.com/Think-Positively',
+        'https://www.wikihow.com/Improve-Critical-Thinking-Skills',
+        'https://www.wikihow.com/Teach-Critical-Thinking',
         'https://www.wikihow.com/Deal-with-Mind-Games'
     ]
 
     def parse(self, response):
         title = response.css('#section_0 a::text').get()
-        heading = response.css('.in-block .mw-headline::text').getall()
+        headings = response.css('.in-block .mw-headline::text').getall()
         body = response.css('.whb::text').getall()
         steps = response.css('.step_num::text').getall()
 
-        positions = np.argwhere(np.array(steps) == '1')
-        positions = [x[0] for x in positions]
+        text = str('[{}]({})\n===\n').format(title, response.url)
+        for heading in headings:
+            text += heading + '\n---\n'
+            # text += body[0] + '\n'
 
-        print('-'*25)
-        print('-'*25)
-        print(steps)
-        print(positions)
-        print('-'*25)
-        print('-'*25)
+            # i = 1
+            i = 0
+            while True:
+                text += body[i] + '\n'
+                i += 1
+                if (steps[i] == None or steps[i] == '1'):
+                    break
+
+            text += '\n'
+
+        with open('wikihow.txt', 'a+') as f:
+            f.write(text)
